d52469201f57247a8ed7958786198e6222e9e56d
fortunatebit <jon.whitehorn@gmail.com>
2025-05-29 21:09:20 -0400

    added player to the screen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+
+
 
 def main():
     pygame.init()
@@ -9,6 +12,8 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -17,6 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None, special_flags=0)
+
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
